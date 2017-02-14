be21edb32d1ae272020d87289966b753b8b654d1
woosungchu <woosungchu@gmail.com>
2017-02-14 22:28:23 +0900

    debug invalid session check error

--- mindmap-web/app/routes/maps/new.js
+++ mindmap-web/app/routes/maps/new.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend({
+  session : service('session'),
   beforeModel() {
     this._super(...arguments);
     if (!this.get('session.isAuthenticated')) {
